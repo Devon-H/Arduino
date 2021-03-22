e0300cb3c6abb256b9c1a6336fd84d6ab54dbece
Devon-H <60160379+Devon-H@users.noreply.github.com>
2021-03-21 19:13:56 -0700

    Added HX711 driver functionality and clean up

--- Impeller_Balance_Jig_Motor_Controller/Impeller_Balance_Jig_Motor_Controller.ino
+++ Impeller_Balance_Jig_Motor_Controller/Impeller_Balance_Jig_Motor_Controller.ino
@@ -1,7 +1,10 @@
 // Include Wire Library for I2C
 #include <Wire.h>
+
+//Load Cell Library
+#include <HX711_ADC.h>
  
-// Include Adafruit Graphics & OLED libraries
+//Adafruit Graphics & OLED libraries
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
@@ -9,12 +12,27 @@
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
-//yourmom
+//Load pins
+const int HX711_dout_1 = 4; //mcu > HX711 no 1 dout pin
+const int HX711_sck_1 = 5; //mcu > HX711 no 1 sck pin
+const int HX711_dout_2 = 6; //mcu > HX711 no 2 dout pin
+const int HX711_sck_2 = 7; //mcu > HX711 no 2 sck pin
+
+
+//HX711 constructor
+HX711_ADC LoadCell_1(HX711_dout_1, HX711_sck_1);
+HX711_ADC LoadCell_2(HX711_dout_2, HX711_sck_2); //HX711 2
+
+const float calibrationValue_1 = 42069; // calibration value load cell 1
+const float calibrationValue_2 = 277680; // calibration value load cell 2
+
+unsigned long t = 0;
+
 //initialize motor pin and PWM value
 int mpin = 3;
 float mval =  0;
 
-float dutyCycle = 0.00; 
+float dutyCycle = 0; 
 
 //initalize potentiometer pin and value
 int ppin = A3;
@@ -30,27 +48,11 @@ float dt;
 float rpm;
 bool read_rpm;
 
-void setup() {
-  // put your setup code here, to run once:
-
-  // Start Wire library for I2C
-  Wire.begin();
-
-  // initialize OLED with I2C addr 0x3C
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  
-  
-  Serial.begin(115200); //initialize serial by setting serial baud (communication rate)
-  pinMode(hall, INPUT_PULLUP); //set hall sensor pin as an input with integrated pullup resistor
-  initial = millis(); //set initial to time since program start in milliseconds
-   
-}
-
-
 void displayRPMandDC(float rpm, float DC){
 
-  Serial.print(String(rpm) +" ");
-  Serial.print(String(DC));
+  //Serial.print(String(rpm) +" ");
+  //Serial.print(String(DC));
+  
   // Clear the display
   display.clearDisplay();
   //Set the color - always use white despite actual display color
@@ -67,12 +69,70 @@ void displayRPMandDC(float rpm, float DC){
   display.print("%");
 }
 
+void setup() {
+  // put your setup code here, to run once:
+
+  // Start Wire library for I2C
+  Wire.begin();
+
+  // initialize OLED with I2C addr 0x3C
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  
+  Serial.begin(115200); //initialize serial by setting serial baud (communication rate)
+  
+  pinMode(hall, INPUT_PULLUP); //set hall sensor pin as an input with integrated pullup resistor
+  initial = millis(); //set initial to time since program start in milliseconds
+
+  //load cell setup
+  LoadCell_1.begin();
+  LoadCell_2.begin();
+
+  unsigned long stabilizingtime = 2000; // tare preciscion can be improved by adding a few seconds of stabilizing time
+  boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
+  byte loadcell_1_rdy = 0;
+  byte loadcell_2_rdy = 0;
+  while ((loadcell_1_rdy + loadcell_2_rdy) < 2) { //run startup, stabilization and tare, both modules simultaniously
+    if (!loadcell_1_rdy) loadcell_1_rdy = LoadCell_1.startMultiple(stabilizingtime, _tare);
+    if (!loadcell_2_rdy) loadcell_2_rdy = LoadCell_2.startMultiple(stabilizingtime, _tare);
+  }
+  if (LoadCell_1.getTareTimeoutFlag()) {
+    Serial.println("Timeout, check MCU>HX711 no.1 wiring and pin designations");
+  }
+  if (LoadCell_2.getTareTimeoutFlag()) {
+    Serial.println("Timeout, check MCU>HX711 no.2 wiring and pin designations");
+  }
+  LoadCell_1.setCalFactor(calibrationValue_1); // user set calibration value (float)
+  LoadCell_2.setCalFactor(calibrationValue_2); // user set calibration value (float)
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
+
+  static boolean newDataReady = 0;
+  const int serialPrintInterval = 0; //increase value to slow down serial print activity
+
+  // check for new data/start next conversion:
+  if (LoadCell_1.update() || LoadCell_2.update()) newDataReady = true;
   
-  pval = analogRead(ppin); //
+
+  //get smoothed value from data set
+  if ((newDataReady)) {
+    if (millis() > t + serialPrintInterval) {
+      float a = LoadCell_1.getData();
+      float b = LoadCell_2.getData();
+      Serial.print(a);
+      Serial.print(",");
+      Serial.print(b);
+      Serial.println(",");
+      
+      newDataReady = 0;
+      t = millis();
+    }
+  }
+
+  pval = analogRead(ppin); //read potentiometer value
   mval = map(pval, 0, 1023, 0, 255); //map the potentiometer position to PWM value
-  analogWrite(mpin, mval);
+  analogWrite(mpin, mval); //write PWM value to speed control mosfet
 
   dutyCycle = mval/255*100; //calculate duty cycle percentage
 
@@ -86,9 +146,9 @@ void loop() {
     dutyCycle = mval/255*100; //calculate duty cycle percentage
     
     //print to serial monitor for debugging
-    Serial.print("RPM = " + String(rpm)); 
-    Serial.print("potentiometer = " + String(pval) + " ");
-    Serial.println("motor = " + String(mval)+ " ");
+    //Serial.print("RPM = " + String(rpm)); 
+    //Serial.print("potentiometer = " + String(pval) + " ");
+    //Serial.println("motor = " + String(mval)+ " ");
      
     //display to OLED
     displayRPMandDC(rpm, dutyCycle);
@@ -107,9 +167,9 @@ void loop() {
   rpm = 60000/dt; //calculate RPM
 
   //print to serial monitor for debugging
-  Serial.print("RPM = " + String(rpm) + " "); 
-  Serial.print("potentiometer = " + String(pval) + " ");
-  Serial.println("motor = " + String(mval));
+  //Serial.print("RPM = " + String(rpm) + " "); 
+  //Serial.print("potentiometer = " + String(pval) + " ");
+  //Serial.println("motor = " + String(mval));
 
   //display to OLED
   displayRPMandDC(rpm, dutyCycle);
